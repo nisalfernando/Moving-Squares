27a7c37e246dd0b656cf10023af4353af6d4116e
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-11-25 19:37:58 +0100

    Configuring the wbepack

--- src/js/views/squares.js
+++ src/js/views/squares.js
@@ -2,4 +2,5 @@
 export default function changeColor() {
     let canvas = document.querySelector("#squareCanvas");
     canvas.style.backgroundColor = '#D0D0D0';
+    canvas.style.alignContent = 'left';
 }
